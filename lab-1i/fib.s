ca2db82a3785dc146b1b1cbce3f1d577d7079045
Tim Liu <tim.yw.liu@gmail.com>
2015-07-18 19:46:37 +0800

    fix the used register

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -13,36 +13,32 @@ fibonacci:
 	@ PROLOG
 	push {r3, r4, r5, lr}
 
-	@ R4 = R0 - 0 (update flags)
 	@ if(R0 <= 0) goto .L3 (which returns 0)
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
+	@ If R0 == 1 goto .L4 (which returns 1)
 
 	cmp r0, #1
 	beq .L4
 	blt .L3
 
-	mov r4, r0	@i=N
-	mov r1, #0	@a=0
-	mov r2, #1	@b=1
+	@ i = N - 1
+	@ a = 0
+	@ b = 1
+	sub r5, r0, #1
+	mov r3, #0
+	mov r4, #1
+
+	@ sum = a + b
+	@ a = b
+	@ b = sum
+	@ i--
+	@ if (i > 0) goto fib_forloop
 fib_forloop:
-	add r0, r1, r2	@sum=a+b
-	mov r1, r2	@a=b
-	mov r2, r0	@b=sum
-	sub r4, r4, #1	@i--
-	cmp r4, #1
-	bgt fib_forloop	@if (i>1) goto fib_forloop
-
-
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
+	add r0, r3, r4
+	mov r3, r4
+	mov r4, r0
+	subs r5, r5, #1
+	bgt fib_forloop
 
 	pop {r3, r4, r5, pc}		@EPILOG
 
